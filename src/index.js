13f2390829905cef164237ac28219fd81d8ea9b0
Miguel Ferreira <miguel.ferreira@webtales.fr>
2022-03-03 16:34:45 +0000

    fix: Solution example

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } = require("@apollo/client");
-import fetch from "node-fetch";
+const { ApolloClient, createHttpLink } = require('@apollo/client/core');
+const { InMemoryCache } = require('@apollo/client/cache');
 
-// const client = new ApolloClient({
-//   link: createHttpLink({
-//     uri: "https://cloud.ampize.me/api/graphql",
-//     headers: {
-//       access_token: "bcjqegmkqytmfxwololj",
-//     },
-//     fetch,
-//   }),
-//   cache: new InMemoryCache(),
-// });
+const fetch = require('node-fetch');
 
-const client = new ApolloClient({
-  uri: "https://cloud.ampize.me/api/graphql",
-  cache: new InMemoryCache(),
+const gql = require('graphql-tag');
+
+const noCache = {
+    watchQuery: { fetchPolicy: 'no-cache' },
+    query: { fetchPolicy: 'no-cache' },
+    mutate: { fetchPolicy: 'no-cache' }
+  };
+
+const apolloClient = new ApolloClient({
+    link: createHttpLink({
+        uri: "https://cloud.ampize.me/api/graphql?access_token=bcjqegmkqytmfxwololj",
+        fetch: fetch
+    }), 
+    cache: new InMemoryCache({
+        addTypename: false
+    }),
+    defaultOptions: noCache,
 });
 
-client
-  .query({
-    query: gql`
-      query {
+
+apolloClient.query({
+    query: gql(`query {
         contactsList(start: 0, limit: 10) {
           ampizeId
           contentURL
@@ -33,42 +37,9 @@ client
           mail
           numero
         }
-      }
-    `,
-  })
-  .then((contacts) => {
-    console.log(contacts);
-  });
-
-// import axios from "axios";
-// import fs from "fs";
-
-// const baseUrl = "https://cloud.ampize.me/api/graphql?query=query%20q";
-// const token = "&access_token=bcjqegmkqytmfxwololj";
-
-// const queryContactsList =
-//   "{contactsList(start:0,limit:1000){ampizeId%20contentURL%20hasChanges%20isPublished%20%20nom%20prenom%20service}}";
-
-// const contactsListUrl = baseUrl + queryContactsList + token;
-
-// const getContacts = async () => {
-//   try {
-//     const { data } = await axios.get(contactsListUrl);
-//     const contactsList = data.data.contactsList;
-
-//     contactsList.forEach((contact) => {
-//       contact.details = getContactDetails(contact.ampizeId);
-//     });
-
-//     const contactsListJSON = JSON.stringify(data.data.contactsList);
-
-//     fs.writeFile("src/data/contacts.json", contactsListJSON, "utf8", (err) => {
-//       if (err) throw err;
-//       console.log("Fichier créé !");
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// getContacts();
+      }`),
+}).then(data => {
+   console.log(data)
+}).catch(err => {
+    console.log(err);
+});
